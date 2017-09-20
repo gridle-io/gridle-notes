3b1dfdfc6cdd3ebb872f4cb489c5e22736594f94
karan_17 <karnendra_singh@gridle.in>
2017-09-20 14:42:31 +0530

    database migrations

---
+++ database/migrations/2017_09_20_073203_create_checklist_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChecklistTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checklist', function (Blueprint $table) {
+            $table->integer('note_id')->unsigned();
+            $table->foreign('note_id')->references('note_id')->on('notes');
+            $table->increments('checkbox_id');
+            $table->string('label');
+            $table->boolean('is_checked');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checklist');
+    }
+}
